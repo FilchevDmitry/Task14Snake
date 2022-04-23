ca6285fb70284e6ebd3697f946e02f06d347e915
Filchev_Dmitry <filchev_dmitry@mail.ru>
2022-04-23 08:54:01 +0300

    'ok'

--- Task14Snake/Task14Snake.cpp
+++ Task14Snake/Task14Snake.cpp
@@ -2,15 +2,18 @@
 using namespace std;
 int main()
 {	
+
 	const int r = 5;
 	const int c = 5;
     int snake[c][r];
-	for (size_t i = 0; i < 5; i++)
+	int s = -1;
+	for (size_t i = 0; i < c; i++)
 	{
 		for (size_t j = 0; j < r; j++)
 		{
-			(i % 2 == 0)?snake[i][j] = i * r + (j + 1):snake[i][j] = (i * r - j) + r;
+			snake[i][(r-1) * (i % 2) - j * s] = i * r + j;
 		}
+		s *= -1;
 	}
 	for (size_t i = 0; i < r; i++)
 	{
@@ -18,7 +21,7 @@ int main()
 		{
 			cout << snake[i][j];
 		}
-		
+		cout << endl;
 	}
 }
 
